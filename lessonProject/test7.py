55a67d4768468b0e00d10f84731fde069d02e21f
Haoock <lhh859965224@163.com>
2022-06-28 15:32:05 +0800

    add modules

--- lessonProject/test7.py
+++ lessonProject/test7.py
@@ -1,10 +1,13 @@
-def make_pizza(*toppings):
-    """打印顾客点的所有配料"""
-    print(toppings)
+# def make_pizza(*toppings):
+#     """打印顾客点的所有配料"""
+#     print(toppings)
+#
+#
+# while True:
+#     name = input("Please enter a pizza name!")
+#     make_pizza(name)
+#     if name == 'q':
+#         break
 
 
-while True:
-    name = input("Please enter a pizza name!")
-    make_pizza(name)
-    if name == 'q':
-        break
+
--- python_learn/test.py
+++ python_learn/test.py
@@ -3,6 +3,28 @@
 # @Time    : 2021/12/27 11:25 上午
 # @Function:
 
-str = "fds fdsa adsf fdas adsf fdsa"
-for x in str:
-    print(x)
+# candidate_dict = {'h1': 3, 'h2': 4, 'h3': 5}
+# theme = max(candidate_dict, key=lambda x: candidate_dict[x])
+# print(theme)
+#
+# a = [["1", "2"], ["3", "4"], ["5", "6"]]
+# t = []
+# [t.extend(i) for i in a]
+# print(t)
+from websocket import create_connection
+import json
+
+# ws = create_connection("ws://127.0.0.1:8009/websocket/2001")
+ws = create_connection("ws://1.117.166.56:8001/websocket/220101")
+# # 发送数据
+message = {}
+message["username"] = "2001"
+message["content"] = "hello world"
+json_str = json.dumps(message)
+ws.send(json_str)
+# 接收数据
+result = ws.recv()
+print(result)
+# 关闭
+ws.close()
+
